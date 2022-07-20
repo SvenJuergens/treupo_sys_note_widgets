0f593dfcb8fdfef43006e8018d4d3c8b45f33e3e
PKuhlmay <info@treupo.de>
2022-07-20 14:49:28 +0200

    Initial setup

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Sys Note Widgets',
+    'description' => 'A collection of widgets which shows a list of sys_notes',
+    'category' => 'be',
+    'author' => 'Philipp Kuhlmay',
+    'state' => 'alpha',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0 - 11.9.99',
+            'dashboard' => '11.5.0 - 11.9.99',
+            'seo' => '11.5.0 - 11.9.99'
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Treupo\\SysNoteWidgets' => 'Classes'
+        ]
+    ],
+];
---
+++ ext_localconf.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+call_user_func(static function () {
+    if (TYPO3_MODE === 'BE') {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+            '
+module.tx_dashboard {
+    view {
+        templateRootPaths.557676000 = EXT:sys_note_widgets/Resources/Private/Templates/
+        partialRootPaths.557676000 = EXT:sys_note_widgets/Resources/Private/Partials/
+        layoutRootPaths.557676000 = EXT:sys_note_widgets/Resources/Private/Layouts/
+    }
+}'
+        );
+    }
+});
