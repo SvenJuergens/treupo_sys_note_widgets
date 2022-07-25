44ceddc7ca4204afaa0c03d6ff72a1aee1b6e5d6
PKuhlmay <info@treupo.de>
2022-07-25 16:40:37 +0200

    Release 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,13 +4,13 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'A collection of widgets which shows a list of sys_notes',
     'category' => 'be',
     'author' => 'Philipp Kuhlmay',
-    'state' => 'alpha',
-    'version' => '0.1.0',
+    'state' => 'stable',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0 - 11.9.99',
-            'dashboard' => '11.5.0 - 11.9.99',
-            'sys_note' => '11.5.0 - 11.9.99'
+            'typo3' => '10.4.0 - 11.9.99',
+            'dashboard' => '10.4.0 - 11.9.99',
+            'sys_note' => '10.4.0 - 11.9.99'
         ],
         'conflicts' => [
         ],
