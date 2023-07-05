a97d9d5d4ddb45ff7495fa38179b36c68f564358
Philipp Kuhlmay <philipp.kuhlmay@wacon.de>
2023-07-05 09:37:22 +0200

    TYPO3 12 Upgrade

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,21 +1,18 @@
 <?php
 
+use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
+use TYPO3\CMS\Core\Http\ApplicationType;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
 
-call_user_func(static function () {
-    if (TYPO3_MODE === 'BE') {
-        ExtensionManagementUtility::addTypoScriptSetup(
-            '
-module.tx_dashboard {
+defined('TYPO3') or die();
+
+ExtensionManagementUtility::addTypoScriptSetup(
+'module.tx_dashboard {
     view {
         templateRootPaths.557676000 = EXT:treupo_sys_note_widgets/Resources/Private/Templates/
         partialRootPaths.557676000 = EXT:treupo_sys_note_widgets/Resources/Private/Partials/
         layoutRootPaths.557676000 = EXT:treupo_sys_note_widgets/Resources/Private/Layouts/
     }
 }'
-        );
-    }
-});
+);
+
